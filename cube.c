6c7f5ae0e83dc0efe48a2559c3492b6c4302be5c
srikanthpragada <srikanthpragada@gmail.com>
2024-06-07 17:54:25 +0530

    07-JUN

---
+++ cube.c
@@ -0,0 +1,13 @@
+// Write a program to display cube
+
+#include <stdio.h>
+
+void main()
+{
+  int num, cube; // declare variables
+
+
+    num = 9;  // assignment
+    cube = num * num * num;
+    printf("Cube of %d is %d", num, cube);
+}
---
+++ square.c
@@ -0,0 +1,13 @@
+// Write a program to display square of 15
+
+#include <stdio.h>
+
+void main()
+{
+  int num, square; // declare variables
+
+     num = 99;  // assignment
+     square = num * num;
+
+     printf("Square of %d is %d", num, square);
+}
