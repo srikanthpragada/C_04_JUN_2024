94536a6c71997569684a4d420ae5233d630f85ef
srikanthpragada <srikanthpragada@gmail.com>
2024-07-06 18:57:31 +0530

    06-JUL

---
+++ funs/array_pass_by_ref.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void print(int * arr)
+{
+
+}
+
+void main()
+{
+  int a[5] = {10,20,40,50,60};
+
+
+      print(a);
+
+}
---
+++ funs/struct/circle.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+struct circle
+{
+    int x, y, r;
+};
+
+void main()
+{
+  struct circle c1,c2,c3;
+
+
+}
