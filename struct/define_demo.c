ddbddd02f03707406681608873f23785e21cb680
srikanthpragada <srikanthpragada@gmail.com>
2024-07-10 17:55:45 +0530

    10-JUL

---
+++ struct/define_demo.c
@@ -0,0 +1,28 @@
+// #define to define constants
+
+#define SIZE 50
+
+// Macro
+#define iseven(n)  n % 2 == 0
+
+void main()
+{
+   int a[SIZE];
+   int i;
+
+       if(iseven(i))  // i % 2 == 0
+       {
+
+
+       }
+
+       for(i = 0; i < SIZE; i ++)
+           ;
+
+
+       for(i = 0; i < SIZE; i ++)
+          ;
+
+
+
+}
